33cef26e77a70f202544a68929e77519953802a1
justiandevs <justiandev@gmail.com>
2022-11-06 16:38:10 +0100

    Basic validation setup

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, Length } from 'class-validator';
+
 export class AuthDto {
+  @IsString()
+  @Length(4, 64)
   name: string;
+
+  @IsString()
+  @Length(8, 64)
   password: string;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsString, Length, Max, Min } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(4, 64)
   name: string;
+
+  @IsString()
+  @Length(8, 64)
   password: string;
+
+  @IsEmail()
   email: string;
+
   refreshToken: string;
 }
